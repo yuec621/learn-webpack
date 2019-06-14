c1529c9be8f9a37fafb6793d2c05babe593132c8
yuec621 <yuec621@outlook.com>
2019-06-14 16:17:19 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+let path=require('path')
+let HtmlWebpackPlugin=require('html-webpack-plugin')
+let webpack=require('webpack')
+module.exports={
+ mode:'development',
+
+entry:'./src/index.js',
+module:{
+    noParse:/jquery/,//不去解析jquery中的依赖库
+    rules:[
+        {test:'/\.js$/',
+        use:{
+            loader:'babel-loader',
+            options:{
+                presets:[
+                    '@babel/preset-env',//解析es6
+                    '@babel/preset-react'//解析react语法
+                ]
+            }
+        }
+        
+        }
+    ]
+},
+output:{
+    filename:'bundle.js',
+    path:path.resolve(__dirname,'dist')
+},
+plugins:[
+    new webpack.IgnorePlugin(/\.\/locale/,/moment/),
+    new HtmlWebpackPlugin({
+        template:'./public/index.html'
+    })
+]
+}
