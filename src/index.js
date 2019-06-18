87b0d56f2f2e01e1dcde3df8b196dce3f51cd9d8
yuec621 <yuec621@outlook.com>
2019-06-18 16:46:07 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -1,17 +1,2 @@
-// import jquery from 'jquery'
-// import moment from 'moment'
-// //手动引入所需要的语言
-// import 'moment/locale/zh-cn'
-// moment.locale('zh-cn')
-// let r=moment().endOf('day').fromNow()
-// console.log(r)
-
-let button=document.createElement('button')
-button.innerHTML='hello'
-button.addEventListener('click',function(){
-    // console.log('click')
-    import('./source.js').then(data=>{
-        console.log(data)
-    })
-})
-document.body.appendChild(button)
+let str= require('./a')
+console.log(str)
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,9 @@
 let path=require('path')
-let HtmlWebpackPlugin=require('html-webpack-plugin')
-let webpack=require('webpack')
-//模块happypack可以实现多线程来打包  进程
-// let Happypack=require('happypack')
 module.exports={
- mode:'development',
- entry:'./src/index.js',
-devServer:{
-    port:3000,
-    open:true,
-    contentBase:'./dist'
-},
-module:{
-    noParse:/jquery/,//不去解析jquery中的依赖库
-    rules:[
-    //    {
-    //        test:/\.js$/,
-    //        exclude:/node_modules/,
-    //        include:path.resolve('src'),
-    //        use:'happypack/loader?id=js'
-    //    }
-    ]
-},
-output:{
-    filename:'bundle.js',
-    path:path.resolve(__dirname,'dist')
-},
-plugins:[
-  
-    // new webpack.IgnorePlugin(/\.\/locale/,/moment/),
-    // new Happypack({
-    //     id:'js'
-    // }),
-    new HtmlWebpackPlugin({
-        template:'./public/index.html'
-    })
-]
+    mode:'development',
+    entry:'./src/index.js',
+    output:{
+        filename:'bundle.js',
+        path:path.resolve(__dirname,'dist')
+    }
 }
